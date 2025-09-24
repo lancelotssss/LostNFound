04fc0d431183bf524a50ead18c1eaa25065a7df8
lancelotssss <lance.villegas2002@gmail.com>
2025-09-24 19:36:48 +0800

    Save local changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import LostNFoundForm from './Pages/Report_Item';
+import Claim_Form from './Pages/Claim_Form';
+import RegisterForm from './Pages/RegisterPage';
+
 
 function App() {
   return (
    <>
-   <LostNFoundForm/>
+     <RegisterForm/>
    </>
   );
 }
