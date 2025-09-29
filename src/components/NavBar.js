02237c8c43df70b2eb34e6b114ac6b7e33b8bf1c
lancelotssss <lance.villegas2002@gmail.com>
2025-09-29 22:33:17 +0800

    lance update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AppstoreOutlined, MailOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
+import { logOutUser } from "../api";
 
 export const pageDataClient = [
    {
